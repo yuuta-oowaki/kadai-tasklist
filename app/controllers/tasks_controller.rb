9df36e0a27dc9286ce4201a18cdaebb51af0afac
yuuta.oowaki <oy11222@i.softbank.jp>
2020-10-23 09:16:15 +0000

    kadai1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -13,12 +18,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に投稿されました"
       redirect_to @task
     else
+      @task = current_user.tasks.order(id :desc).page(params[:page])
       flash.now[:danger] = "Task が投稿されませんでした"
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
  
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
