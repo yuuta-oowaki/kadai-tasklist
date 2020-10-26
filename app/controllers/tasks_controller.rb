3f13419613e16ba16a6613b3cf70516ce02f0773
yuuta.oowaki <oy11222@i.softbank.jp>
2020-10-26 14:13:35 +0000

    syuusei3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, except: [:index ,:new]
+  before_action :correct_user, except: [:index, :new]
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
