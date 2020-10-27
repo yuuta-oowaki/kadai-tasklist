21e78316371d4e48be5094e8e24e3027b2289398
yuuta.oowaki <oy11222@i.softbank.jp>
2020-10-27 08:06:41 +0000

    syuusei4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に投稿されました"
